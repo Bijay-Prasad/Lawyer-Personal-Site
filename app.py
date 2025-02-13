51c97431e74bc0f872df711f5fa58111bf24b3ae
Samprit Ghosh <88526732+samprit-ghosh@users.noreply.github.com>
2025-02-13 14:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,19 +157,15 @@ def login():
 
 @app.route('/login/google')
 def login_google():
-    # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
+        redirect_uri="https://lawyer-personal-site.onrender.com/login/google/callback",  # ✅ Correct URL
         scope=["openid", "email", "profile"],
     )
     return redirect(request_uri)
-
 
 @app.route('/login/google/callback')
 def google_authorize():
